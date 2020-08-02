8219b5864262ebd76a40646ed768c86d70dcd562
Hemant <hemant.janrao@wirecard.com>
2020-08-02 20:05:40 +0200

    Updated with cucumber support

--- src/test/java/org/takeaway/stepdefs/ListStepDefinitions.java
+++ src/test/java/org/takeaway/stepdefs/ListStepDefinitions.java
@@ -1,6 +1,7 @@
 package org.takeaway.stepdefs;
 
 import io.cucumber.java.After;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,6 +12,7 @@ import org.takeaway.core.util.Environment;
 import org.takeaway.core.util.PropertyUtils;
 import org.takeaway.server.client.ListServer;
 import org.takeaway.server.entity.model.List;
+import org.takeaway.server.entity.model.UpdateList;
 import org.takeaway.server.entity.response.ListResponse;
 import org.testng.Assert;
 
@@ -29,7 +31,7 @@ public class ListStepDefinitions {
     @When("I create a list with {string} name")
     public void iCreateAListWithName(String listName) {
         var list = new List();
-        list.setName(listName.strip());
+        list.setName(listName);
         list.setIso_639_1("en");
 
         createdList = listServer.createList(TestHelper.serializeToJson(list));
@@ -38,9 +40,9 @@ public class ListStepDefinitions {
                 String.format("List creation with name: %s failed", list.getName()));
     }
 
-    @Then("List should get created {string} result")
-    public void listShouldGetCreatedSuccessfully(boolean status) {
-        if(status){
+    @Then("Should be with {string} result")
+    public void listShouldGetCreatedSuccessfully(String status) {
+        if(status.equalsIgnoreCase("pass")){
         Assert.assertTrue(listServer.doesListExists(createdList.getId()));
         } else {
             Assert.assertFalse(listServer.doesListExists(createdList.getId()));
@@ -53,5 +55,16 @@ public class ListStepDefinitions {
         Assert.assertEquals(response.statusCode(), HttpStatus.SC_OK);
 
         Assert.assertFalse(listServer.doesListExists(createdList.getId()));
+    }
+
+    @And("I update list name to {string}")
+    public void iUpdateListNameToUpdated_name(String updateName) {
+
+        var updateList = new UpdateList();
+        updateList.setDescription("Updated description");
+        updateList.setName(updateName);
+
+        var updatedList = listServer.updateList(createdList.getId(), TestHelper.serializeToJson(updateList));
+        Assert.assertEquals(updatedList.statusCode(), HttpStatus.SC_CREATED);
     }
 }
